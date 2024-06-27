c47c84ad357545563a75925360e67fdc76b1ccc6
Daijon Bourne <Daijonbourne1005@gmail.com>
2024-06-26 21:04:11 -0700

    Fixed routes to properly show when typed into url

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import { HashRouter as Router, Routes, Route } from 'react-router-dom';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import Home from './pages/Home';
 import Vote from './pages/Vote';
 import Ranking from './pages/Ranking';
@@ -10,10 +10,10 @@ function App() {
   return (
     <Router>
       <Routes>
-        <Route path='/' element={<Home/>}/>
-        <Route path='/Vote' element={<Vote/>}/>
-        <Route path='/Ranking' element={<Ranking/>}/>
-        <Route path='/AddCharacter' element={<AddCharacter/>}/>
+        <Route path='/' element={<Home />}/>
+        <Route path='/Vote' element={<Vote />}/>
+        <Route path='/Ranking' element={<Ranking />}/>
+        <Route path='/AddCharacter' element={<AddCharacter />}/>
       </Routes>
     </Router>
   )
--- src/pages/Vote.jsx
+++ src/pages/Vote.jsx
@@ -1,4 +1,7 @@
-export function Vote() {
+import React from "react";
+
+
+function Vote() {
     return (
         <>
             <h1>This is the Vote Page</h1>
