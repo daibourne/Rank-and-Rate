85c4a49b5ef219876420a42e3130226aa1c2a4a8
Daijon Bourne <Daijonbourne1005@gmail.com>
2024-06-26 21:22:58 -0700

    Added links to Nav bar component and made sure they work properly

--- src/components/Nav.jsx
+++ src/components/Nav.jsx
@@ -1,14 +1,15 @@
 import React from "react";
+import { Link } from "react-router-dom";
 import styles from '../styles/Nav.css'
 
 function Navbar() {
     return (
         <div class='navbar'>
             <ul id="nav-list">
-                <li><a href="">Home</a></li>
-                <li><a href="">Vote</a></li>
-                <li><a href="">Ranking</a></li>
-                <li><a href="">Add Character</a></li>
+                <li><Link to={'/'}>Home</Link></li>
+                <li><Link to={'/Vote'}>Vote</Link></li>
+                <li><Link to={'/Ranking'}>Ranking</Link></li>
+                <li><Link to={'/AddCharacter'}>Add Character</Link></li>
             </ul>
         </div>
     );
